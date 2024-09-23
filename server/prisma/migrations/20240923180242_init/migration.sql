ac3fc908962abd5c52bc6932e4689827665c5415
Shaul1KR <shaulikr07@gmail.com>
2024-09-23 21:29:37 +0300

    Migration

--- server/prisma/migrations/20240923180242_init/migration.sql
+++ server/prisma/migrations/20240923180242_init/migration.sql
@@ -3,12 +3,7 @@ CREATE TABLE "Url" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "shortUrl" TEXT NOT NULL,
     "longUrl" TEXT NOT NULL,
-    "accessCount" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Url_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Url_shortUrl_key" ON "Url"("shortUrl");
--- server/src/controllers/url.controller.ts
+++ server/src/controllers/url.controller.ts
@@ -1,21 +1,33 @@
 import { Request, Response } from "express";
-import { generateShortUrl } from "../utils/urlService";
 import { prisma } from "..";
+import { decode, encode } from "../utils/urlService";
 
 export const createShortUrl = async (req: Request, res: Response) => {
   try {
-    const { longUrl } = req.body;
+    const { longUrl } = req.body as { longUrl: string };
+    console.info(`Create a shot url with ${longUrl} url`);
 
     // Validate the input
     if (!longUrl) {
       return res.status(400).json({ error: "longUrl is required" });
     }
-
+    const { protocol, baseUrl, headers } = req;
+    const { host } = headers;
+    const longUrlData = await prisma.url.upsert({
+      where: { longUrl },
+      update: {},
+      create: { longUrl },
+    });
+    const shortUrl = encode(longUrlData.id);
     // Generate or retrieve the short URL
-    const shortUrl = await generateShortUrl(longUrl);
+
+    console.log(req.baseUrl);
 
     // Send response with the newly created short URL
-    return res.status(201).json({ longUrl, shortUrl });
+    return res.status(201).json({
+      longUrl,
+      shortUrl: `${protocol}://${host}${baseUrl}/${shortUrl}`,
+    });
   } catch (error) {
     console.error("Error creating short URL:", error);
     return res
@@ -27,10 +39,12 @@ export const createShortUrl = async (req: Request, res: Response) => {
 export const redirectToLongUrl = async (req: Request, res: Response) => {
   try {
     const { shortUrl } = req.params;
+    console.info("Redirect to long url");
 
     // Find the URL entry in the database using the shortUrl
+    const id = decode(shortUrl);
     const urlEntry = await prisma.url.findUnique({
-      where: { shortUrl },
+      where: { id },
     });
 
     // If the short URL does not exist, return a 404 error
@@ -39,6 +53,7 @@ export const redirectToLongUrl = async (req: Request, res: Response) => {
     }
 
     // Redirect the user to the long URL
+
     return res.redirect(urlEntry.longUrl);
   } catch (error) {
     console.error("Error during redirection:", error);
