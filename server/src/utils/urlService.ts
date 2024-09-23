90fcd9e14527c3d28c87e5ff3da1233b98bc5f5d
Shaul1KR <shaulikr07@gmail.com>
2024-09-23 21:30:35 +0300

    Change decode and encode

--- server/src/utils/urlService.ts
+++ server/src/utils/urlService.ts
@@ -1,43 +1,20 @@
-// urlService.ts
-import { prisma } from "../index";
-
-const base62Chars =
+const characters =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const base = characters.length;
 
 // Function to convert a number to Base62
-const encodeBase62 = (num: number): string => {
+export const encode = (num: number): string => {
   let shortUrl = "";
   while (num > 0) {
-    shortUrl = base62Chars[num % 62] + shortUrl;
-    num = Math.floor(num / 62);
+    shortUrl = shortUrl + characters[num % base];
+    num = Math.floor(num / base);
   }
-  return shortUrl || "0"; // Ensure at least '0' is returned
+  return shortUrl || "a"; // Ensure at least 'a' is returned
 };
 
-// Function to generate a unique short URL
-export const generateShortUrl = async (longUrl: string): Promise<string> => {
-  // Use `findFirst` to check if the long URL already exists
-  const existingUrl = await prisma.url.findFirst({
-    where: { longUrl },
-  });
-
-  if (existingUrl) {
-    return existingUrl.shortUrl; // Return existing short URL if it exists
-  }
-
-  // If it doesn't exist, count the current URLs to generate a new short URL
-  const count = await prisma.url.count(); // Get the current count of URLs
-
-  // Generate a new short URL based on the count
-  const newShortUrl = encodeBase62(count + 1); // Increment count for new entry
-
-  // Save the new long-url and short-url mapping to the database
-  await prisma.url.create({
-    data: {
-      longUrl,
-      shortUrl: newShortUrl,
-    },
-  });
-
-  return newShortUrl; // Return the newly created short URL
+export const decode = (shortUrl: string) => {
+  return shortUrl
+    .split("")
+    .map((letter) => characters.indexOf(letter))
+    .reduce((prev, current, index) => prev + current * base ** index, 0);
 };
